22cfb396b405bd40b531f17547ed78fbf72f81fb
wuxuweilalala <2509165479@qq.com>
2021-01-18 09:34:37 +0800

    课程详情

--- src/controllers/lesson.ts
+++ src/controllers/lesson.ts
@@ -1,21 +1,31 @@
-import {Lesson, LessonDocument} from "../models";
-import {Request,Response} from "express";
+import { Lesson, LessonDocument } from "../models";
+import { Request, Response } from "express";
 
-export const list = async (req:Request,res:Response)=>{
-  let {limit = 0,offset = 5,category = 'all'} = req.query;
-  offset =  Number(offset);
+export const list = async (req: Request, res: Response) => {
+  let { limit = 0, offset = 5, category = 'all' } = req.query;
+  offset = Number(offset);
   limit = Number(limit);
-  let query:any = {};
-  if(category && category !== 'all') {
+  let query: any = {};
+  if (category && category !== 'all') {
     query.category = category
   }
   let total = await Lesson.count(query); // 符合查询条件的总条数
-  const list:LessonDocument[] = await Lesson.find(query).sort({order:1}).skip(offset).limit(limit);
+  const list: LessonDocument[] = await Lesson.find(query).sort({ order: 1 }).skip(offset).limit(limit);
   res.json({
-    success:true,
-    data:{
+    success: true,
+    data: {
       list,
-      hasMore:total > offset + limit
+      hasMore: total > offset + limit
     }
+  })
+}
+
+export const getLesson = async (req: Request, res: Response) => {
+  let id = req.params.id;
+
+  const list: LessonDocument = await Lesson.findById(id)
+  res.json({
+    success: true,
+    data: list
   })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Express, Request,Response,NextFunction} from 'express';   // 启动服务
+import express, { Express, Request, Response, NextFunction } from 'express';   // 启动服务
 import mongoose from 'mongoose'; // 用来连接数据库
 /*import cors from 'cors';         // 解决跨域
 import morgan from "morgan";     // 输出访问日志
@@ -8,7 +8,7 @@ import path from "path";
 import 'dotenv/config';          // 读取 .env 然后写入 process.env
 import errorMiddleware from "./middleware/errorMiddleware";
 import HttpException from "./exception/HttpException";
-import {Lesson, Slider} from './models';
+import { Lesson, Slider } from './models';
 import * as UserController from './controllers/user'
 import * as SliderController from './controllers/slider'
 import * as LessonController from './controllers/lesson'
@@ -18,82 +18,83 @@ import bodyParser from "body-parser";
 // 指定上传文件的存储空间
 const storage = multer.diskStorage({
     // 指定上传的目录
-    destination: path.join(__dirname,'public','uploads'),
-    filename(_req:Request,file:Express.Multer.File,callback) {
-        callback(null,Date.now()+path.extname(file.originalname))
+    destination: path.join(__dirname, 'public', 'uploads'),
+    filename(_req: Request, file: Express.Multer.File, callback) {
+        callback(null, Date.now() + path.extname(file.originalname))
     }
 });
-const upload =multer({storage})
+const upload = multer({ storage })
 
-const app:Express = express();
+const app: Express = express();
 /*app.use(cors);
 app.use(morgan('dev'));
 app.use(helmet);*/
-app.use(express.static(path.join(__dirname,'public')));
-app.all('*', function(req, res, next) {
+app.use(express.static(path.join(__dirname, 'public')));
+app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Authorization,Accept,X-Requested-With');
-    res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
     res.header('X-Powered-By', '3.2.1')
-    if(req.method === 'OPTIONS') {
+    if (req.method === 'OPTIONS') {
         res.send(200);
     } else {
         next();
     }
 });
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(_req,res,_next)=>{
+app.get('/', (_req, res, _next) => {
     res.json({
-        success:true,
-        data:'hello world'
+        success: true,
+        data: 'hello world'
     });
 });
 // 注册接口
-app.post('/user/register',UserController.register);
+app.post('/user/register', UserController.register);
 // 登录接口
-app.post('/user/login',UserController.login);
-app.get('/user/validate',UserController.validate)
-app.post('/user/uploadAvatar',upload.single('avatar'), UserController.uploadAvatar)
+app.post('/user/login', UserController.login);
+app.get('/user/validate', UserController.validate)
+app.post('/user/uploadAvatar', upload.single('avatar'), UserController.uploadAvatar)
 
-app.get('/sliders/list',SliderController.list)
-app.get('/lessons/list',LessonController.list)
+app.get('/sliders/list', SliderController.list)
+app.get('/lessons/list', LessonController.list)
+app.get('/lesson/:id', LessonController.getLesson)
 
 // 没有匹配到任何路由，则创建一个自定义404错误对象并传递给错误处理中间件
-app.use((_req:Request,_res:Response,next:NextFunction)=>{
-    const error:HttpException = new HttpException(404,'尚未为此路径分配路由')
+app.use((_req: Request, _res: Response, next: NextFunction) => {
+    const error: HttpException = new HttpException(404, '尚未为此路径分配路由')
     next(error)
 });
 
 app.use(errorMiddleware);
 
 
-(async function() {
-    await mongoose.set('useNewUrlParser',true);
-    await mongoose.set('useUnifiedTopology',true);
+(async function () {
+    await mongoose.set('useNewUrlParser', true);
+    await mongoose.set('useUnifiedTopology', true);
     const MONGODB_URL = process.env.MONGODB_URL || `mongodb://localhost/wuxuwei`;
     await mongoose.connect(MONGODB_URL);
     await createInitialSliders();
     await createInitialLessons();
     const PORT = process.env.PORT || 8003;
-    app.listen(PORT,()=>{
+    app.listen(PORT, () => {
         console.log(`Running on http://localhost:${PORT}`)
     })
 })()
 
 async function createInitialSliders() {
     const sliders = await Slider.find();
-    if(sliders.length === 0) {
+    if (sliders.length === 0) {
         const sliders = [
             {
-                url:'https://imgsa.baidu.com/forum/w%3D580/sign=815c232d5f3d26972ed3085565fab24f/940b0ff2d7ca7bcb2bf6d9c8b0096b63f724a84a.jpg',
+                url: 'https://imgsa.baidu.com/forum/w%3D580/sign=815c232d5f3d26972ed3085565fab24f/940b0ff2d7ca7bcb2bf6d9c8b0096b63f724a84a.jpg',
             },
             {
-                url:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2805577034,826713399&fm=26&gp=0.jpg',
+                url: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2805577034,826713399&fm=26&gp=0.jpg',
             },
             {
-                url:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3665917667,4181822611&fm=26&gp=0.jpg',
+                url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3665917667,4181822611&fm=26&gp=0.jpg',
             }
         ]
         await Slider.create(sliders)
@@ -102,7 +103,7 @@ async function createInitialSliders() {
 
 async function createInitialLessons() {
     const lessons = await Lesson.find();
-    if(lessons.length === 0) {
+    if (lessons.length === 0) {
         const lessons = [
             {
                 order: 1,
